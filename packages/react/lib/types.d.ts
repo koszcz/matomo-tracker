fc588e74a88513809067be72a60a906c912d0002
Juan González <juan.gonzalez@femtasy.com>
2022-01-18 13:40:49 +0100

    Temporarily include built packages

---
+++ packages/react/lib/types.d.ts
@@ -0,0 +1,16 @@
+import MatomoTracker, { types } from '@datapunt/matomo-tracker-js';
+export interface MatomoInstance {
+    trackEvent: MatomoTracker['trackEvent'];
+    trackEvents: MatomoTracker['trackEvents'];
+    trackPageView: MatomoTracker['trackPageView'];
+    trackSiteSearch: MatomoTracker['trackSiteSearch'];
+    trackLink: MatomoTracker['trackLink'];
+    pushInstruction: MatomoTracker['pushInstruction'];
+    setDisabled: MatomoTracker['setDisabled'];
+}
+export declare type InstanceParams = types.UserOptions;
+export declare type TrackPageViewParams = types.TrackPageViewParams;
+export declare type TrackEventParams = types.TrackEventParams;
+export declare type TrackSiteSearchParams = types.TrackSiteSearchParams;
+export declare type TrackLinkParams = types.TrackLinkParams;
+//# sourceMappingURL=types.d.ts.map
